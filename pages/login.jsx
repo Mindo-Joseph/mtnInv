5d7848ef9f011c73792d0976090b76815b0f65e1
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-02-28 16:15:48 +0300

    user can sign up and sign in

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,24 @@
+import React, {useState} from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { supabase } from "../lib/supabaseClient.js";
+
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      alert(JSON.stringify(error));
+    } else {
+      router.push("/");
+    }
+  }
   return (
     <div className="bg-gray-100 min-h-screen">
       <Head>
@@ -15,7 +32,7 @@ const LoginPage = () => {
           <h2 className="text-2xl font-extrabold text-gray-900">
             Sign in to your account
           </h2>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
@@ -30,6 +47,7 @@ const LoginPage = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Email address"
+                  onChange={(event) => setEmail(event.target.value)}
                 />
               </div>
               <div>
@@ -44,59 +62,39 @@ const LoginPage = () => {
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Password"
+                  onChange={(event) => setPassword(event.target.value)}
                 />
               </div>
             </div>
 
             <div className="flex items-center justify-between mt-6">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
+
 
               <div className="text-sm">
-                <Link href="/forgot-password">
-                  <p className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Forgot your password?
-                  </p>
+                <Link
+                  href="/forgot-password"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot your password?
                 </Link>
               </div>
             </div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
 
-            <form>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+
+              <Link
+                href="/signup"
+                className="text-center font-medium text-indigo-600 hover:text-indigo-500"
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.293 7.293a1 1 0 011.414 0L10 12.586l5.293-5.293a1 1 0 011.414 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                Sign in
-              </button>
+                Sign up instead
+              </Link>
             </form>
-          </form>
+
         </div>
       </div>
     </div>
