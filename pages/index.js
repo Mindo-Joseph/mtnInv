852be704d7d9f5b197fdd839a8bde1e465394346
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-02-28 12:46:53 +0300

    create homepage

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,32 @@
 import React from "react";
 
-import {supabase} from './../lib/supabaseClient.js';
+import Head from 'next/head';
+import Link from 'next/link';
 
-function Page({countries}) {
-    return (<ul> {
-        countries.map((country) => (<li key={
-            country.id
-        }> {
-            country.name
-        }</li>))
-    } </ul>);
-}
+const HomePage = () => {
+    return (<div className="bg-gray-100 min-h-screen">
+        <Head>
+            <title>Inventory Management App</title>
+            <meta name="description" content="A modern inventory management app."/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
 
-export async function getServerSideProps() {
-    let {data} = await supabase.from('countries').select()
+        <div className="max-w-5xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-extrabold text-gray-900">Inventory Management App</h1>
+            <p className="mt-4 text-lg text-gray-500">Keep track of your inventory with ease.</p>
+<p className = "mt-4 text-lg text-gray-500"> Sell to your local community</p>
 
-    return {
-        props: {
-            countries: data
-        }
-    }
-}
+            <div className="mt-8">
+                <Link href="/login">
+                    <p className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                        Sign in to your account
+                    </p>
+                </Link>
+            </div>
+        </div>
+    </div>);
+};
 
-export default Page;
+export default HomePage;
+
 
